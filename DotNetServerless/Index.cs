2fdf27c139d9d195289304cb2a7e3c5b34c60af1
Will James <wjames@thoughtworks.com>
2017-09-07 22:08:18 +1000

    Adding logging

--- DotNetServerless/Index.cs
+++ DotNetServerless/Index.cs
@@ -5,13 +5,25 @@ using System.IO;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Amazon.Lambda.Serialization.Json;
+using Serilog;
+using Serilog.Formatting.Json;
 
 [assembly:LambdaSerializer(typeof(JsonSerializer))]
 
 namespace DotNetServerless {
   public class Hello {
+    ILogger logger;
+
+    public Hello() {
+      logger = new LoggerConfiguration()
+        .WriteTo.Console(new JsonFormatter())
+        .CreateLogger();
+    }
+
     public APIGatewayProxyResponse Handler(APIGatewayProxyRequest request)
     {
+      var log = logger.ForContext("RequestId", request.RequestContext.RequestId);
+      log.Information("Entering Hello::Handler");
       return APIGatewayHelper.Success(new HelloBody());
     }
 
--- DotNetServerlessTests/IndexTests.cs
+++ DotNetServerlessTests/IndexTests.cs
@@ -10,16 +10,22 @@ namespace DotNetServerlessTests
     public class HelloTests
     {
         private readonly Hello _hello;
+        private readonly APIGatewayProxyRequest _request;
 
         public HelloTests()
         {
             _hello = new Hello();
+            _request = new APIGatewayProxyRequest {
+                RequestContext = new APIGatewayProxyRequest.ProxyRequestContext {
+                    RequestId = "test-id"
+                }
+            };
         }
 
         [Fact]
         public void HelloHandlerShouldReturn200()
         {
-            var response = _hello.Handler(new APIGatewayProxyRequest());
+            var response = _hello.Handler(_request);
 
             Assert.Equal(200, response.StatusCode);
         }
@@ -27,7 +33,7 @@ namespace DotNetServerlessTests
         [Fact]
         public void HelloHandlerShouldReturnHelloWorldMessage()
         {
-          var response = _hello.Handler(new APIGatewayProxyRequest());
+          var response = _hello.Handler(_request);
 
           Assert.Equal("{\"Message\":\"Hello World!\"}", response.Body);
         }
@@ -35,7 +41,7 @@ namespace DotNetServerlessTests
         [Fact]
         public void HelloHandlerResponseShouldBeSerializable()
         {
-          var response = _hello.Handler(new APIGatewayProxyRequest());
+          var response = _hello.Handler(_request);
 
           MemoryStream stream = new MemoryStream();
           new JsonSerializer().Serialize(response, stream);
